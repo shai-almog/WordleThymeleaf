60607ddd716697606c5b0eceeeaa43ed564b6e3b
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2023-03-29 11:34:22 +0300

    Adding session support

--- src/main/java/com/debugagent/wordle/web/WebController.java
+++ src/main/java/com/debugagent/wordle/web/WebController.java
@@ -2,6 +2,7 @@ package com.debugagent.wordle.web;
 
 import com.debugagent.wordle.service.WordleService;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +13,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
+@Scope("session")
 @AllArgsConstructor
 public class WebController {
     private final WordleService wordleService;
 
-    private static List<String> attempts = new ArrayList<>();
+    private List<String> attempts = new ArrayList<>();
 
     @GetMapping("/guessMVC")
     public String guess(Model model) {
+        showAttempts(model);
         return "WordleGuess";
     }
 
@@ -32,11 +35,15 @@ public class WebController {
             model.addAttribute("errorMessage", "");
             attempts.add(guess);
         }
+        showAttempts(model);
+
+        return "WordleGuess";
+    }
+
+    private void showAttempts(Model model) {
         List<WebResult[]> results = attempts.stream()
                 .map(str -> WebResult.create(wordleService.calculateResults(str), str))
                 .collect(Collectors.toList());
         model.addAttribute("entries", results);
-
-        return "WordleGuess";
     }
 }
